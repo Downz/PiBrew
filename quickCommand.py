03cfdff9bf740170c756d11b2a992b8dfc34fb10
downz <andersmh@privat.dk>
2021-01-09 22:37:37 +0100

    Updated quickCommand - now works when program is terminated too

--- quickCommand.py
+++ quickCommand.py
@@ -1,12 +1,30 @@
 import serial
 import time
 import sys
- 
+
+sleeptime = 1.5
+
 ser = serial.Serial('/dev/ttyACM0', 115200)
+
+ser.write('test'.encode())
+
+def printAll():
+    while ser.in_waiting:
+        print(ser.readline().decode())
+
+
 command = str(sys.argv[1]).encode()
 print(command)
 
+time.sleep(sleeptime)
+
+printAll()
+
 ser.write(command)
+
 while not ser.in_waiting:  # Or: while self.ser.inWaiting():
     pass
-print(ser.readline().decode())
+
+time.sleep(sleeptime)
+
+printAll()
